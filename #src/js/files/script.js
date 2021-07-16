f0669d083a001cb69b5bc8b8587136f54813ee17
mitriifd <idimka9@gmail.com>
2021-07-17 00:30:48 +0500

    update

---
+++ #src/js/files/script.js
@@ -0,0 +1,84 @@
+$(function () {
+	 $('.top-slider').slick({
+		 
+			slidesToShow: 1,
+			infinite: false,
+			variableWidth: true,
+			arrows: false,
+			responsive: [
+				{
+					breakpoint: 1170,
+					settings: {
+						centerMode: true,
+						variableWidth: true,
+						slidesToShow: 3,
+					},
+				},
+				{
+					breakpoint: 960,
+					settings: {
+						centerMode: true,
+						variableWidth: true,
+						slidesToShow: 3,
+					},
+				},
+
+				{
+					breakpoint: 321,
+					variableWidth: false,
+					settings: {
+						slidesToShow: 2,
+					},
+				},
+			],
+	});
+
+	$('.product-slider').slick({
+		slidesToShow: 5,
+		infinite: false,
+		variableWidth: true,
+		arrows: true,
+		waitForAnimate: false,
+		appendArrows: $('.product-slider__controls'),
+		// responsive: [
+		// 	{
+		// 		breakpoint: 1170,
+		// 		settings: {
+		// 			centerMode: true,
+		// 			variableWidth: true,
+		// 			slidesToShow: 3,
+		// 		},
+		// 	},
+		// 	{
+		// 		breakpoint: 960,
+		// 		settings: {
+		// 			centerMode: true,
+		// 			variableWidth: true,
+		// 			slidesToShow: 3,
+		// 		},
+		// 	},
+
+		// 	{
+		// 		breakpoint: 321,
+		// 		variableWidth: false,
+		// 		settings: {
+		// 			slidesToShow: 2,
+		// 		},
+		// 	},
+		// ],
+	})
+
+	// $('.product-slider__button-prev').click(function (event) {
+	// 	$('.product-slider').slick('slickPrev')
+	// })
+
+	// $('.product-slider__button-next').click(function (event) {
+	// 	$('.product-slider').slick('slickNext')
+	// })
+
+
+});
+
+	
+
+
