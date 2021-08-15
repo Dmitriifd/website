cc7f63747a91f9bed16bf3a655dda657c5613861
mitriifd <idimka9@gmail.com>
2021-08-15 21:01:00 +0500

    add new site

---
+++ optimeal/#src/js/files/script.js
@@ -0,0 +1,17 @@
+$('.vitamins__slider').slick({
+    
+    fade: true,
+});
+
+
+// const test = document.querySelector('.test');
+// const testЕitle = document.querySelector('.test-title');
+
+// if (testЕitle.clientWidth < 200) {
+//     test.style.width = 200 + 'px';
+// } else if (testЕitle.clientWidth > 200) {
+//     test.style.width = testЕitle.clientWidth + 'px';
+// }
+
+// console.log(testЕitle.clientWidth);
+// console.log(test.clientWidth);
