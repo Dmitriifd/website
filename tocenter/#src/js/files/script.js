7c941fabe72cc0c32ee66069002c220fdb82853c
mitriifd <idimka9@gmail.com>
2021-08-28 22:25:15 +0500

    adaptive header and footer

---
+++ tocenter/#src/js/files/script.js
@@ -0,0 +1,18 @@
+
+const headerIcon = document.querySelector('.header__icon')
+const headerIconClose = document.querySelector('.header__icon-close')
+const headerIconOpen = document.querySelector('.header__icon-open')
+const menuBody = document.querySelector('.menu__body')
+const menuFooter = document.querySelector('.menu__footer')
+
+
+headerIcon.addEventListener('click', (e) => {
+    e.preventDefault()
+    headerIcon.classList.toggle('_active')
+    headerIconOpen.classList.toggle('_active')
+    headerIconClose.classList.toggle('_active')
+    menuBody.classList.toggle('_active')
+    menuFooter.classList.toggle('_active')
+})
+
+
