1469f9f50ca69b7520a6e9efb4fd2bf13741f353
mitriifd <idimka9@gmail.com>
2021-09-01 22:14:43 +0500

    update

--- tocenter/#src/js/files/script.js
+++ tocenter/#src/js/files/script.js
@@ -1,18 +1,24 @@
 
-const headerIcon = document.querySelector('.header__icon')
-const headerIconClose = document.querySelector('.header__icon-close')
-const headerIconOpen = document.querySelector('.header__icon-open')
-const menuBody = document.querySelector('.menu__body')
-const menuFooter = document.querySelector('.menu__footer')
+const headerIcon = document.querySelector('.header__icon');
+const headerIconClose = document.querySelector('.header__icon-close');
+const headerIconOpen = document.querySelector('.header__icon-open');
+const menuBody = document.querySelector('.menu__body');
+const menuFooter = document.querySelector('.menu__footer');
 
 
 headerIcon.addEventListener('click', (e) => {
-    e.preventDefault()
-    headerIcon.classList.toggle('_active')
-    headerIconOpen.classList.toggle('_active')
-    headerIconClose.classList.toggle('_active')
-    menuBody.classList.toggle('_active')
-    menuFooter.classList.toggle('_active')
+    e.preventDefault();
+    headerIcon.classList.toggle('_active');
+    headerIconOpen.classList.toggle('_active');
+    headerIconClose.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    menuFooter.classList.toggle('_active');
+
+    if (headerIcon.classList.contains('_active')) {
+        document.body.style.overflow = 'hidden';
+    } else {
+        document.body.style.overflow = '';
+    }
 })
 
 
