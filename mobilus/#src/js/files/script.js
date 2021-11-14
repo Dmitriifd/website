7f00a31d5f38ac66a5cdb036bd95b89c873ea371
mitriifd <idimka9@gmail.com>
2021-11-14 21:22:54 +0500

    add slider for new section

--- mobilus/#src/js/files/script.js
+++ mobilus/#src/js/files/script.js
@@ -53,3 +53,62 @@ const heroSlider = new Swiper('.hero__slider', {
     // },
     
 })
+
+const newSlider = new Swiper('.new__slider', {
+    // Бесконечный режим прокрутки слайдов
+    loop: true,
+
+    // Скорость прокрутки
+    speed: 2000,
+
+    // Переключение клавишами
+    keyboard: {
+        enabled: true,
+        onlyInViewport: true,
+    },
+
+    // Стрелки
+    navigation: {
+        nextEl: '.new__slider-button--next',
+        prevEl: '.new__slider-button--prev',
+    },
+
+    // Дотсы
+    pagination: {
+        el: ".new__slider-pagination",
+        clickable: true,
+    },
+
+    // Количество слайдов
+    slidesPerView: 4,
+
+    // Автовысота
+    autoHeight: true,
+
+    // Отступ между слайдами
+    spaceBetween: 24,
+
+    // Автопрокрутка
+    // autoplay: {
+    //     // Пауза между прокруткой
+    //     delay: 5000,
+    //     // Закончить на последнем слайде
+    //     stopOnLastSlide: false,
+    //     // Отключить после ручного переключения
+    //     disableOnInteraction: false
+    // },
+
+    // Брейк поинты (адаптив)
+    // breakpoints: {
+    //     320: {
+    //         slidesPerView: 1,
+    //     },
+    //     480: {
+    //         slidesPerView: 2,
+    //     },
+    //     992: {
+    //         slidesPerView: 3,
+    //     }
+    // },
+
+})
