e6d825e9d3adb3bc3125304375be41800dd2c12f
mitriifd <idimka9@gmail.com>
2021-11-17 17:25:39 +0500

    add filter

--- mobilus/#src/js/files/forms.js
+++ mobilus/#src/js/files/forms.js
@@ -502,19 +502,19 @@ if (priceSlider) {
 	noUiSlider.create(priceSlider, {
 		start: [0, 200000],
 		connect: true,
-		tooltips: [wNumb({ decimals: 0, prefix: textFrom + ' ' }), wNumb({ decimals: 0, prefix: textTo + ' ' })],
+		tooltips: [wNumb({ decimals: 0, }), wNumb({ decimals: 0,  })],
 		range: {
 			'min': [0],
-			'max': [200000]
+			'max': [10000]
 		}
 	});
 
-	/*
+
 	const priceStart = document.getElementById('price-start');
 	const priceEnd = document.getElementById('price-end');
 	priceStart.addEventListener('change', setPriceValues);
 	priceEnd.addEventListener('change', setPriceValues);
-	*/
+	
 
 	function setPriceValues() {
 		let priceStartValue;
