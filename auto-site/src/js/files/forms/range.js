61e7e047cfaa17c4f7f3b0fd6b566c9c2bb839e4
mitriifd <idimka9@gmail.com>
2022-06-04 23:21:19 +0500

    секция credit верстка без адаптива

--- auto-site/src/js/files/forms/range.js
+++ auto-site/src/js/files/forms/range.js
@@ -10,12 +10,12 @@ import '../../libs/wNumb.min.js';
 
 export function rangeInit() {
 	const priceSlider = document.querySelector('#range');
+	let sliderSum = document.querySelector('#slider-sum');
+	const sliderTime = document.querySelector('#slider-time');
 
 	if (priceSlider) {
-		let textFrom = priceSlider.getAttribute('data-from');
-		let textTo = priceSlider.getAttribute('data-to');
 
-		var nonLinearStepSliderValueElement =
+		const nonLinearStepSliderValueElement =
 			document.getElementById('slider-value');
 
 		noUiSlider.create(priceSlider, {
@@ -46,41 +46,94 @@ export function rangeInit() {
 		lineValue[1].textContent = '500т';
 
 		priceSlider.noUiSlider.on('update', function (values) {
-            const value = values.map(function (value) {
-                return Number(value).toFixed(0)
-            })
-            
-            value[1] < 1000 ? nonLinearStepSliderValueElement.innerHTML = value.join(' - ') + 'т' : nonLinearStepSliderValueElement.innerHTML = value.join(' - ') + 'м';
-            
-			
+			const value = values.map(function (value) {
+				return Number(value).toFixed(0);
+			});
+
+			value[1] < 1000
+				? (nonLinearStepSliderValueElement.innerHTML = value.join(' - ') + 'т')
+				: (nonLinearStepSliderValueElement.innerHTML = value.join(' - ') + 'м');
+		});
+	}
+
+	if (sliderSum) {
+		
+
+		// let sliderSumValue = document.document.querySelector('.range-slider__value');
+
+		noUiSlider.create(sliderSum, {
+			start: [0],
+			connect: true,
+			step: 500,
+
+			range: {
+				min: [0],
+				max: 3000,
+			},
+			pips: {
+				mode: 'steps',
+				density: -1,
+				format: wNumb({
+					suffix: 'м',
+					decimals: 1,
+					thousand: ',',
+					encoder: function (value) {
+						return value >= 1000 ? value / 1000 : value;
+					},
+				}),
+			},
 		});
 
-		// var skipValues = [
-		// 	document.querySelector('.noUi-handle-lower'),
-		// 	document.querySelector('.noUi-handle--upper'),
-		// ];
+      
+      
 
-		// priceSlider.noUiSlider.on('update', function (values, handle) {
-		// 	skipValues[handle].innerHTML = values[handle];
-		// });
+		let lineValue = document.querySelectorAll('.slider-range .noUi-value');
+		lineValue[0].textContent = 0;
+		lineValue[1].textContent = '500т';
 
-		/*
-		const priceStart = document.getElementById('price-start');
-		const priceEnd = document.getElementById('price-end');
-		priceStart.addEventListener('change', setPriceValues);
-		priceEnd.addEventListener('change', setPriceValues);
-		*/
-		// function setPriceValues() {
-		// 	let priceStartValue;
-		// 	let priceEndValue;
-		// 	if (priceStart.value != '') {
-		// 		priceStartValue = priceStart.value;
-		// 	}
-		// 	if (priceEnd.value != '') {
-		// 		priceEndValue = priceEnd.value;
-		// 	}
-		// 	priceSlider.noUiSlider.set([priceStartValue, priceEndValue]);
-		// }
+
+		var pips = sliderSum.querySelectorAll('.noUi-value');
+
+		function clickOnPip() {
+			var value = Number(this.getAttribute('data-value'));
+			sliderSum.noUiSlider.set(value);
+		}
+
+		for (var i = 0; i < pips.length; i++) {
+			// For this example. Do this in CSS!
+			pips[i].style.cursor = 'pointer';
+			pips[i].addEventListener('click', clickOnPip);
+		}
+	}
+	if (sliderTime) {
+		noUiSlider.create(sliderTime, {
+			connect: true,
+
+			range: {
+				min: 6,
+				'12.5%': 12,
+				'27%': 24,
+				'41.5%': 36,
+				'56%': 48,
+				'70.5%': 60,
+				'85%': 72,
+				max: 84,
+			},
+			start: [6],
+			step: 12,
+			pips: { mode: 'steps' },
+		});
+		var pips = sliderTime.querySelectorAll('.noUi-value');
+
+		function clickOnPip() {
+			var value = Number(this.getAttribute('data-value'));
+			sliderTime.noUiSlider.set(value);
+		}
+
+		for (var i = 0; i < pips.length; i++) {
+			pips[i].style.cursor = 'pointer';
+			pips[i].addEventListener('click', clickOnPip);
+		}
 	}
 }
 rangeInit();
