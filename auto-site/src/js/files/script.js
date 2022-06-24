c57e09785b689f200bf3722b8f5ca97042f1fd7e
Dmitriifd <idimka9@gmail.com>
2022-06-24 22:44:53 +0500

    hamburger menu for desktop

--- auto-site/src/js/files/script.js
+++ auto-site/src/js/files/script.js
@@ -1,5 +1,17 @@
 // Подключение функционала "Чертогов Фрилансера"
-import { isMobile } from "./functions.js";
+import { isMobile } from './functions.js';
 // Подключение списка активных модулей
-import { flsModules } from "./modules.js";
+import { flsModules } from './modules.js';
 
+const menuIcon = document.querySelector('.menu__icon');
+const hamburgerMenu = document.querySelector('.hamburger-menu');
+
+menuIcon.addEventListener('click', () => {
+	hamburgerMenu.classList.toggle('active');
+	if (hamburgerMenu.classList.contains('active')) {
+        document.body.style.overflow = 'hidden';
+	} else {
+         document.body.style.overflow = '';
+    }
+
+});
